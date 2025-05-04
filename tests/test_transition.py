d1e5f8e0c5653ee14bd4eb110560aa9d72e14064
Elena Ermolenkova <e-wolf@rambler.ru>
2025-05-04 23:24:23 +0300

    Переименовала тестовые файлы

---
+++ tests/test_transition.py
@@ -0,0 +1,24 @@
+import pytest
+
+from locators import Locators
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from data import Credentials
+
+
+@pytest.mark.parametrize("locator", [
+    Locators.LOGO,
+    Locators.CONSTRUCTOR
+    ])
+def test_transition_from_personal_cabinet(driver, locator):
+    driver.find_element(*Locators.PERSONAL_CABINET).click()
+    WebDriverWait(driver, 5).until(EC.url_to_be("https://stellarburgers.nomoreparties.site/login"))
+    driver.find_element(*Locators.EMAIL).send_keys(Credentials.email)
+    driver.find_element(*Locators.PASSWORD).send_keys(Credentials.password)
+    driver.find_element(*Locators.LOGIN).click()
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(Locators.PERSONAL_CABINET))
+    driver.find_element(*Locators.PERSONAL_CABINET).click()
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(locator))
+    driver.find_element(locator).click()
+    WebDriverWait(driver, 10).until(EC.url_to_be("https://stellarburgers.nomoreparties.site"))
+    assert driver.current_url
