50da8aeaf83b367b7c485b8547b5fe7d076b9d63
Elena Ermolenkova <e-wolf@rambler.ru>
2025-05-07 21:36:31 +0300

    Исправление файла test_exit.py

--- tests/test_exit.py
+++ tests/test_exit.py
@@ -5,16 +5,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from data import Credentials
 from curl import *
 
-def test_exit(driver):
-    driver.find_element(*Locators.PERSONAL_CABINET).click()
-    WebDriverWait(driver, 10).until(EC.url_to_be(login_page))
-    driver.find_element(*Locators.EMAIL).send_keys(Credentials.email)
-    driver.find_element(*Locators.PASSWORD).send_keys(Credentials.password)
-    driver.find_element(*Locators.LOGIN).click()
-    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(Locators.PERSONAL_CABINET))
-    driver.find_element(*Locators.PERSONAL_CABINET).click()
-    WebDriverWait(driver, 5).until(EC.url_to_be(account_page))
-    driver.find_element(*Locators.EXIT).click()
-    WebDriverWait(driver, 10).until(EC.url_to_be(login_page))
-    current_url = driver.current_url
-    assert login_page in current_url
+class TestExit:
+
+    def test_exit(self, driver):
+        driver.find_element(*Locators.PERSONAL_CABINET).click()
+        WebDriverWait(driver, 10).until(EC.url_to_be(login_page))
+        driver.find_element(*Locators.EMAIL).send_keys(Credentials.email)
+        driver.find_element(*Locators.PASSWORD).send_keys(Credentials.password)
+        driver.find_element(*Locators.LOGIN).click()
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable(Locators.PERSONAL_CABINET))
+        driver.find_element(*Locators.PERSONAL_CABINET).click()
+        WebDriverWait(driver, 5).until(EC.url_to_be(account_page))
+        driver.find_element(*Locators.EXIT).click()
+        WebDriverWait(driver, 10).until(EC.url_to_be(login_page))
+        current_url = driver.current_url
+        assert login_page in current_url
